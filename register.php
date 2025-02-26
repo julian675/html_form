7909da4dde5bdeadadc02e8194fd5d4191a3b247
julian675 <jalamares23-0376@cca.edu.ph>
2025-02-26 17:57:39 +0800

    register update
    fixed my mistakes

--- register.php
+++ register.php
@@ -1,25 +1,32 @@
-<!DOCTYPE html>
 <html>
-    <head>
-        <title>Javascript Form</title>
-    </head>
-    <body>
-
-        <form action="action_page.php" method="POST">
-
-            <fieldset>
-                <legend>Person:</legend>
-                <label for="fname">First name:</label>
-                <input type="text" id="fname" name="fname"><br><br>
-                <label for="lname">Last name:</label>
-                <input type="text" id="lname" name="lname"><br><br>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email"><br><br>
-                <label for="birthday">Birthday:</label>
-                <input type="date" id="birthday" name="birthday"><br><br>
-                <input type="submit" value="SAVE RECORD" name="submit_record" />
-            </fieldset>
-        </form>
-
-    </body>
+    <form action="action.php" method="POST">
+        <table border="1">
+            <tr>
+                <td> Name: </td>
+                <td> <input type="text" name="name"> </td>
+            </tr>
+            <tr>
+                <td> Gender: </td>
+                <td>
+                    <input type="radio" name="gender" value="male"> Male
+                    <input type="radio" name="gender" value="female"> Female
+                </td>
+            </tr>
+            <tr>
+                <td> Country: </td>
+                <td>
+                    <select name="country">
+                        <option value="PH">PH</option>
+                        <option value="AUS">AUS</option>
+                        <option value="US">US</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" value="Save">
+                </td>
+            </tr>
+        </table>
+    </form>
 </html>
